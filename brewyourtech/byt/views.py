474360a83e3e7ffb81df0222df9abb4a439f1828
Tapple214 <111862212+Tapple214@users.noreply.github.com>
2024-12-17 01:30:19 +0800

    Setup
    tested object fetching
    
    installed bootstrap
    
    rearranged the directory
    
    removed unnecessary files/folders
    
    files and folders added
    
    added byt files and folders
    
    Initial commit

---
+++ brewyourtech/byt/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from .models import *
+
+# Create your views here.
+
+# Index/Home page
+def index(request):
+    response_string = User.objects.all()[0]
+    return render(request, 'byt/index.html', {'user_data': response_string})
+
+# Login page
+def login(request):
+    return render(request, 'byt/login.html')
