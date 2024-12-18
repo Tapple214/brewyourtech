60284a842e2560acb73871f60f33dece2e7bb851
Tapple214 <applegate21441@gmail.com>
2024-12-18 16:19:24 +0800

    brewery ui and page

--- brewyourtech/byt/urls.py
+++ brewyourtech/byt/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('navbar', views.navbar, name='navbar'),
     path('login', views.login, name='login'),
-    path('', views.index, name='index'),
+    path('', views.brewLog, name='brewLog'),
+    path('brewery', views.brewery, name='brewery'),
 ]
--- brewyourtech/byt/views.py
+++ brewyourtech/byt/views.py
@@ -4,16 +4,20 @@ from .models import *
 
 # Create your views here.
 
-# Index/Home page
-def index(request):
+# Index/Home page aka "Brew Log"
+def brewLog(request):
     response_string = User.objects.all()[0]
-    return render(request, 'byt/index.html', {'user_data': response_string})
+    return render(request, 'byt/brewLog.html', {'user_data': response_string})
 
 # Login page
 def login(request):
     return render(request, 'byt/login.html')
 
 
-# Login page
+# Navbar component
 def navbar(request):
     return render(request, 'byt/navbar.html')
+
+# Assembly/Filer page aka "Brewery"; assembly means where we "assemble" our wanted device
+def brewery(request):
+    return render(request, 'byt/brewery.html')
