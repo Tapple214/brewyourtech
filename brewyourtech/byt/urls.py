bb943e20d9fa15fdf52155b0a3fef23682fdd006
Tapple214 <applegate21441@gmail.com>
2024-12-19 14:33:14 +0800

    added commens to urls

--- brewyourtech/byt/urls.py
+++ brewyourtech/byt/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+# To access on browser eg. path('login', views.login, name='login') > /login will show template in views.login
+
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path('login', views.login, name='login'), 
     path('', views.brewLog, name='brewLog'),
     path('brewery', views.brewery, name='brewery'),
 ]
